6345c866f5667b2e0e428ded89a15c537de75aa2
Kevin Maillet <kevinrmaillet@gmail.com>
2021-12-15 18:34:58 -0500

    added polyfill for gatsby-image on safari

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,10 @@ import { SiteProvider } from './src/context/siteContext';
 export const wrapRootElement = ({ element }) => (
   <SiteProvider>{element}</SiteProvider>
 );
+
+//Polyfill for Gatsby-image on Safari browsers
+export const onClientEntry = async () => {
+  if (typeof IntersectionObserver === 'undefined') {
+    await import('intersection-observer');
+  }
+};
