50e1b7d02e0f9e0e0bf07ebf773a16a05594a1e1
kevin maillet <kevinmaillet@kevins-MacBook-Pro.local>
2022-01-13 20:45:56 -0500

    updated ideas page

--- src/pages/ideas.js
+++ src/pages/ideas.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 
-const Ideas = () => {
+const Ideas = ({ data }) => {
+
+  const { content } = data.wpPost;
+
   return (
     <Layout>
       <Seo title="Ideas" />
-
       <section className="ideas">
         <h2 className="ideas__title">Ideas</h2>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: content,
+          }}
+        />
       </section>
     </Layout>
   );
 };
+
+export const query = graphql`
+query getIdeasPost {
+  wpPost(title: {eq: "Ideas"}) {
+    id
+    content
+  }
+}
+
+`;
 
 export default Ideas;
